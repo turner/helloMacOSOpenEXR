5edb3c6d8d0f754955f6675b600ba46449fb68b8
Douglass Turner <douglass.turner@gmail.com>
2016-12-18 15:55:47 -0500

    Initial Commit

---
+++ helloMacOSOpenEXR/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  helloMacOSOpenEXR
+//
+//  Created by Douglass Turner on 12/18/16.
+//  Copyright © 2016 Elastic Image Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
